7990cd4d961a5e1bb5fb934dd83498ace6c0031c
muyildir <muyildir@stud.hs-heilbronn.de>
2015-03-18 11:52:18 +0100

    Gesamtcommit

--- app/src/main/java/com/example/kemalyildirim/myapplication/MainActivity.java
+++ app/src/main/java/com/example/kemalyildirim/myapplication/MainActivity.java
@@ -25,7 +25,7 @@ public class MainActivity extends ActionBarActivity {
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
                     .add(R.id.container, new PlaceholderFragment())
-                    .commit();
+                    .commit(); //jkjk
         }
     }
 
